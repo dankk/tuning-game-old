7a08263667873b60faeb294d4c87bdf2491f7fe1
Dan K <dan.khalid3@gmail.com>
2020-05-17 18:43:28 -0400

    restart button

--- api/utils/noteManager.js
+++ api/utils/noteManager.js
@@ -1,6 +1,6 @@
 const tunings = {
   //string 1 - 6
-  standard: [28, 23, 19, 14, 9, 4]
+  standard: [28, 23, 19, 14, 9, 4],
 };
 
 const tones_list = [
@@ -15,29 +15,29 @@ const tones_list = [
   "G#",
   "A",
   "A#",
-  "B"
+  "B",
 ];
 
 const octs_list = [2, 3, 4, 5];
 
 const notesList = octs_list.reduce((notes, oct) => {
-  const oct_notes = tones_list.map(n => `${n}${oct}`);
+  const oct_notes = tones_list.map((n) => `${n}${oct}`);
   return [...notes, ...oct_notes];
 }, []);
 
-const indexToNoteFile = idx => {
+const indexToNoteFile = (idx) => {
   const soundFileName = notesList[idx].replace("#", "s");
   return `/sound-files/${soundFileName}.mp3`;
 };
 
 module.exports = {
-  getStartingData: difficulty => {
+  getStartingData: (difficulty) => {
     console.log(difficulty);
 
     const randRange = 3; // +/- this many notes
 
     let randIndex = [];
-    let startingNotes = tunings.standard.map(v => v);
+    let startingNotes = tunings.standard.map((v) => v);
     for (let i = 0; i < difficulty; i++) {
       var r = Math.floor(Math.random() * 6);
       while (randIndex.includes(r)) {
@@ -48,7 +48,7 @@ module.exports = {
     console.log(randIndex);
     console.log(startingNotes);
 
-    randIndex.forEach(v => {
+    randIndex.forEach((v) => {
       const n = startingNotes[v];
       let diff = Math.floor(Math.random() * randRange * 2) - randRange;
       while (diff === 0) {
@@ -58,22 +58,10 @@ module.exports = {
     });
     console.log(startingNotes);
 
-    // const doRand = () => Math.floor(Math.random() * 2); //0 or 1
-    // let startingNotes = tunings.standard.map(v =>
-    //   doRand() ? v + Math.floor(Math.random() * randRange * 2) - randRange : v
-    // );
-    // if (startingNotes.toString() === tunings.standard.toString()) {
-    //   let change = Math.floor(Math.random() * randRange * 2) - randRange;
-    //   while (change === 0) {
-    //     change = Math.floor(Math.random() * randRange * 2) - randRange;
-    //   }
-    //   //force wrong note
-    //   startingNotes[Math.floor(Math.random() * 6)] += change;
-    // }
     return {
       correctNotes: tunings.standard,
       startingNotes,
-      notesList: notesList
+      notesList: notesList,
     };
-  }
+  },
 };
--- app/src/components/App.js
+++ app/src/components/App.js
@@ -10,11 +10,11 @@ import { indexToNoteFile } from "../utils/noteHandler";
 
 const useStyle = makeStyles({
   root: {
-    textAlign: "-webkit-center"
-  }
+    textAlign: "-webkit-center",
+  },
 });
 
-const getStartingData = async difficulty => {
+const getStartingData = async (difficulty) => {
   const res = await fetch(
     `http://localhost:5000/start?difficulty=${difficulty}`
   );
@@ -32,11 +32,18 @@ function App() {
   const [started, setStarted] = useState(false);
   const [difficulty, setDifficulty] = useState(null);
 
+  const doRestart = () => {
+    setStartingData(null);
+    setRound({ value: 0 });
+    setDifficulty(null);
+    setStarted(false);
+  };
+
   const handleNoteChange = (stringIdx, noteIdx) => {
     selectedNotes = [
       ...selectedNotes.slice(0, stringIdx),
       noteIdx,
-      ...selectedNotes.slice(stringIdx + 1)
+      ...selectedNotes.slice(stringIdx + 1),
     ];
     //play note
     const newSound = indexToNoteFile(noteIdx, startingData.notesList);
@@ -47,12 +54,12 @@ function App() {
   useEffect(() => {
     async function fetchData(difficulty) {
       await getStartingData(difficulty)
-        .then(res => {
+        .then((res) => {
           setStartingData(res);
           selectedNotes = res.startingNotes;
           correctNotes = res.correctNotes;
         })
-        .catch(error => console.log(error));
+        .catch((error) => console.log(error));
     }
     if (difficulty) {
       fetchData(difficulty);
@@ -60,13 +67,10 @@ function App() {
   }, [round, difficulty]);
 
   const doSubmit = useCallback((selectedNotes, correctNotes) => {
-    //hacky ?
     const submitResult = handleSubmit(selectedNotes, correctNotes);
     const setRoundFunc = () => {
-      console.log("in setroundfunc");
-      console.log(submitResult);
       submitResult
-        ? setRound(s => ({ value: s.value + 1 }))
+        ? setRound((s) => ({ value: s.value + 1 }))
         : setRound({ value: 0 });
     };
     setTimeout(() => {
@@ -75,11 +79,6 @@ function App() {
     }, 2000);
     return submitResult;
   }, []);
-
-  // if (!startingData) {
-  //   console.log("nulling");
-  //   return null;
-  // }
 
   if (!started) {
     return (
@@ -92,6 +91,9 @@ function App() {
   if (startingData) {
     return (
       <Container className={classes.root}>
+        <Button variant="outlined" onClick={() => doRestart()}>
+          Restart
+        </Button>
         <div>Difficulty: {difficulty}</div>
         <div>Score: {round.value}</div>
         <StringGroup {...startingData} handleNoteChange={handleNoteChange} />
